216b1d67fcedf58e30b3a0d0b1eb31d77769c024
Oswin Justine Ayonoadu <oswinjustine@gmail.com>
2022-07-06 05:45:57 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10 - function to print abc 10 times
  *
@@ -6,14 +7,12 @@
  */
 void print_alphabet_x10(void)
 {
-	char c, i;
-	:wq
-		for (i = 0; i < 9; i++)
-		{
-			for (c = 'a'; c <= 'z'; c++)
-			{
-				_putchar(c);
-			}
-			_putchar('\n');
-		}
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar('\n');
+	}
 }
