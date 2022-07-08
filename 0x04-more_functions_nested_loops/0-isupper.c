eaad913b19446f7fe4d675daaec03ffbb7e7f357
Oswin Justine Ayonoadu <oswinjustine@gmail.com>
2022-07-07 19:59:34 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,22 +1,14 @@
 #include "main.h"
 
 /**
- * _isupper - A function that checks for uppercase character.
- * @c: An input character
- * Return: 1 if c is uppercase or 0 otherwise
+ * _isupper - checks if the character is uppercase
+ * @c: is the int used for the argument of the function.
+ * Return: 1 if uppercase character 0 if not
  */
 int _isupper(int c)
 {
-char uppercase = 'A';
-int isupper = 0;
-
-for (; uppercase <= 'Z'; uppercase++)
-{
-if (c == uppercase)
-{
-isupper = 1;
-break;
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
-}
-
-return (isupper);
